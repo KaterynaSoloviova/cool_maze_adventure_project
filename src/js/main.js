168bb32e56c5334cb6ade69ed2426abad0f07038
Kateryna Soloviova <kateryna.soloviova@gmail.com>
2025-06-30 16:01:49 +0200

    feat: handle a user finish game event; add timer; make the maze bigger

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
 class Maze {
-    constructor(grid) {
+    constructor(grid, timeRemaining) {
         this.grid = grid;
         this.fishCounter = 0;
+        this.timeRemaining = timeRemaining;
     }
 
     displayMaze() {
@@ -9,7 +10,7 @@ class Maze {
         this.grid.forEach((row, i) => {
             row.forEach((element, j) => {
                 const divelement = document.createElement("div");
-                if (element === 0 || element === 2) {
+                if (element === 0 || element === 2 || element === 3) {
                     divelement.setAttribute("class", "o")
                     if (element === 2) {
                         const fish = document.createElement("img");
@@ -18,40 +19,62 @@ class Maze {
                         fish.setAttribute("alt", "fish");
                         fish.setAttribute("id", `fish${i}_${j}`);
                         divelement.appendChild(fish)
+                    } else if (element === 3) {
+                        divelement.classList.add("finish")
                     }
                 } else {
                     divelement.setAttribute("class", "x")
                 }
                 container.appendChild(divelement)
             })
+
         })
     }
 
     isFree(row, col) {
-        return this.grid[row][col] === 0 || this.grid[row][col] === 2;
+        return this.grid[row][col] === 0 || this.grid[row][col] === 2 || this.grid[row][col] === 3;
     }
 
-    isFishThere(row, col){
+    isFishThere(row, col) {
         return this.grid[row][col] === 2
     }
 
-    eatFish(row, col){
+    eatFish(row, col) {
         this.grid[row][col] = 0;
         const fish = document.getElementById(`fish${row}_${col}`);
         fish.classList.add("hidden");
-        this.fishCounter ++;
+        this.fishCounter++;
         const fishScore = document.getElementById("result");
         fishScore.textContent = this.fishCounter;
     }
 
+    checkWin(row, col) {
+        if (this.grid[row][col] === 3 && this.timeRemaining > 0) {
+            document.getElementById("congratsMessage").style.display = "block";
+            clearInterval(timer);
+            return true;
+        }
+        return false
+    }
+
+    
+    checkCurrentPosition(row, col) {
+        if (this.isFishThere(row, col)) {
+            this.eatFish(row, col);
+        }
+        this.checkWin(row, col)
+    }
+
+
+
+
 
 }
 
-
 class Penguin {
     constructor(row, col) {
-        this.width = 50;
-        this.height = 50;
+        this.width = 25;
+        this.height = 25;
         this.row = row;
         this.col = col;
         this.updatedUI();
@@ -60,7 +83,7 @@ class Penguin {
     updatedUI() {
         const penguinElm = document.getElementById("penguin");
         const positionX = this.col * this.width;
-        const positionY = 11 * this.height - this.row * this.height;
+        const positionY = 20 * this.height - this.row * this.height;
 
         penguinElm.style.left = positionX + "px";
         penguinElm.style.bottom = positionY + "px";
@@ -91,53 +114,89 @@ class Penguin {
 
 const penguin = new Penguin(1, 1);
 const maze = new Maze([
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 1, 0, 0, 0, 0, 2, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
-    [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 2, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1],
-    [1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
-    [1, 2, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-]);
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 2, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 2, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 2, 1, 0, 0, 0, 1, 0, 1, 2, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 2, 0, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 2, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 2, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 2, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1],
+    [1, 3, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 2, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 2, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1],
+    [1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 2, 1, 0, 0, 1, 0, 1],
+    [1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 2, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 2, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+], 5);
 
 maze.displayMaze()
 
+let timer;
+function timerHandler() {
+    maze.timeRemaining--;
+    if (maze.timeRemaining > 0) {
+        const minutes = Math.floor(maze.timeRemaining / 60).toString().padStart(2, "0");
+        const seconds = (maze.timeRemaining % 60).toString().padStart(2, "0");
+        const timeRemainingContainer = document.getElementById("timeRemaining");
+        timeRemainingContainer.innerText = `${minutes}:${seconds}`;
+    } else {
+        document.getElementById("gameOverMessage").style.display = "block";
+        clearInterval(timer);
+    }
+}
 
+function startTimer() {
+    if (timer === undefined) {
+        timer = setInterval(timerHandler, 1000);
+    }
+
+}
 document.addEventListener("keydown", (e) => {
     if (e.code === "ArrowLeft") {
+        startTimer()
         if (maze.isFree(penguin.row, penguin.col - 1)) {
             penguin.moveLeft();
-            if (maze.isFishThere(penguin.row, penguin.col)){
-                maze.eatFish(penguin.row, penguin.col);
-            }  
+            maze.checkCurrentPosition(penguin.row, penguin.col)
         }
     } else if (e.code === "ArrowRight") {
+        startTimer()
         if (maze.isFree(penguin.row, penguin.col + 1)) {
             penguin.moveRight();
-            if (maze.isFishThere(penguin.row, penguin.col)){
-                maze.eatFish(penguin.row, penguin.col);
-            }      
+            maze.checkCurrentPosition(penguin.row, penguin.col)
         }
     } else if (e.code === "ArrowUp") {
+        startTimer()
         if (maze.isFree(penguin.row - 1, penguin.col)) {
             penguin.moveUp();
-            if (maze.isFishThere(penguin.row, penguin.col)){
-                maze.eatFish(penguin.row, penguin.col);
-            }  
+            maze.checkCurrentPosition(penguin.row, penguin.col)
         }
     } else if (e.code === "ArrowDown") {
+        startTimer()
         if (maze.isFree(penguin.row + 1, penguin.col)) {
             penguin.moveDown();
-            if (maze.isFishThere(penguin.row, penguin.col)){
-                maze.eatFish(penguin.row, penguin.col);
-            }  
+            maze.checkCurrentPosition(penguin.row, penguin.col)
         }
+
     }
 })
+
+
+
+
+
+
+
+
+
+
+
 
 
